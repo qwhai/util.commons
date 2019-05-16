f76d7c5b641b6ac5674281e9292fc86b88b9e0b7
Q-WHai <return_zero0@163.com>
2019-05-16 17:24:42 +0800

    add DES

---
+++ src/main/java/org/commons/cabinet/encrypt/DES.java
@@ -0,0 +1,72 @@
+package org.commons.cabinet.encrypt;
+
+import org.apache.log4j.Logger;
+import org.commons.cabinet.encrypt.interf.Decrypt;
+import org.commons.cabinet.encrypt.interf.Encrypt;
+
+import javax.crypto.*;
+import javax.crypto.spec.DESKeySpec;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+
+/**
+ * 对称DES加密算法
+ *
+ * DES加密介绍
+ * DES是一种对称加密算法，所谓对称加密算法即：加密和解密使用相同密钥的算法。
+ * DES加密算法出自IBM的研究，后来被美国政府正式采用，之后开始广泛流传，但是近些年使用越来越少，因为DES使用56位密钥，以现代计算能力，24小时内即可被破解。
+ * 虽然如此，在某些简单应用中，我们还是可以使用DES加密算法，本文简单讲解DES的JAVA实现。
+ *
+ * 注意：DES加密和解密过程中，密钥长度都必须是8的倍数
+ *
+ * Create Time: 2019/05/16 17:13
+ * Last Modify: 2019/05/16
+ *
+ * @author Q-WHai
+ * @see <a href="https://github.com/qwhai">https://github.com/qwhai</a>
+ */
+public class DES implements Encrypt, Decrypt {
+
+    private final Logger logger = Logger.getLogger(DES.class);
+
+    private byte[] key = null;
+
+    public void setKey(byte[] key) {
+        this.key = key;
+    }
+
+    @Override
+    public byte[] encrypt(byte[] plain) {
+        return des(plain, Cipher.ENCRYPT_MODE);
+    }
+
+    @Override
+    public byte[] decrypt(byte[] cipher) {
+        return des(cipher, Cipher.DECRYPT_MODE);
+    }
+
+    private byte[] des(byte[] data, int mode) {
+        try {
+            SecureRandom random = new SecureRandom();
+            DESKeySpec desKey = new DESKeySpec(key);
+            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+            SecretKey securekey = keyFactory.generateSecret(desKey);
+            // Cipher对象实际完成加密操作
+            Cipher cipher = Cipher.getInstance("DES");
+            // 用密匙初始化Cipher对象
+            cipher.init(mode, securekey, random);
+            // 正式执行加密操作
+            return cipher.doFinal(data);
+        } catch (InvalidKeyException
+                | NoSuchAlgorithmException
+                | InvalidKeySpecException
+                | NoSuchPaddingException
+                | IllegalBlockSizeException
+                | BadPaddingException ex) {
+            logger.error(ex.getMessage());
+            return null;
+        }
+    }
+}
--- src/test/java/org/commons/cabinet/EncryptTest.java
+++ src/test/java/org/commons/cabinet/EncryptTest.java
@@ -190,4 +190,36 @@ public class EncryptTest extends Testable {
         logger.info(ArrayUtils.output(plain));
         logger.info(new String(plain));
     }
+
+    @Test
+    public void test19() {
+        byte[] key = new byte[]{
+                0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+                0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10
+        };
+
+        Encrypt encrypt = new DES();
+        ((DES) encrypt).setKey(key);
+
+        byte[] cipher = encrypt.encrypt("你好，世界".getBytes());
+        logger.info(ArrayUtils.output(cipher));
+    }
+
+    @Test
+    public void test20() {
+        byte[] cipher = new byte[] {
+                126, -87, 82, -22, -70, 91, 98, -12, 70, 15, 43, 9, -37, 102, -99, 125
+        };
+        byte[] key = new byte[]{
+                0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+                0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10
+        };
+
+        Decrypt decrypt = new DES();
+        ((DES) decrypt).setKey(key);
+
+        byte[] plain = decrypt.decrypt(cipher);
+        logger.info(ArrayUtils.output(plain));
+        logger.info(new String(plain));
+    }
 }
