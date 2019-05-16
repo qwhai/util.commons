8d8ab7141ea7f2ea96b072038f3f9dcda0c23a21
Q-WHai <return_zero0@163.com>
2019-05-16 18:18:27 +0800

    add Rabbit

--- src/test/java/org/commons/cabinet/EncryptTest.java
+++ src/test/java/org/commons/cabinet/EncryptTest.java
@@ -82,7 +82,7 @@ public class EncryptTest extends Testable {
     @Test
     public void test11() {
         String plaintext = "Hello world";
-        byte[] key = new byte[] {
+        byte[] key = {
                 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
                 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10
         };
@@ -91,14 +91,15 @@ public class EncryptTest extends Testable {
         ((SMS4) encrypt).setKey(key);
 
         byte[] cipher = encrypt.encrypt(plaintext.getBytes());
-        if (null == cipher) return;
+        if (null == cipher)
+            return;
         logger.info(ArrayUtils.output(cipher));
     }
 
     @Test
     public void test12() {
         String plaintext = "你好，世界";
-        byte[] key = new byte[] {
+        byte[] key = {
                 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
                 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10
         };
@@ -113,10 +114,10 @@ public class EncryptTest extends Testable {
 
     @Test
     public void test13() {
-        byte[] cipher = new byte[] {
+        byte[] cipher = {
                 -43, -115, -7, -92, 111, 82, 63, -69, 20, -32, 10, 62, -109, -35, 101, -55
         };
-        byte[] key = new byte[]{
+        byte[] key = {
                 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
                 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10
         };
@@ -131,10 +132,10 @@ public class EncryptTest extends Testable {
 
     @Test
     public void test14() {
-        byte[] cipher = new byte[] {
+        byte[] cipher = {
                 96, -87, -49, 31, -125, 29, -62, -74, -96, 1, 127, -8, -52, 107, -82, 8
         };
-        byte[] key = new byte[]{
+        byte[] key = {
                 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
                 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10
         };
@@ -161,7 +162,7 @@ public class EncryptTest extends Testable {
 
     @Test
     public void test17() {
-        byte[] key = new byte[]{
+        byte[] key = {
                 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
                 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10
         };
@@ -175,10 +176,10 @@ public class EncryptTest extends Testable {
 
     @Test
     public void test18() {
-        byte[] cipher = new byte[] {
+        byte[] cipher = {
                 66, -49, -31, 51, 92, 103, -31, 104, -83, -57, 113, 40, 18, -116, -47, -58
         };
-        byte[] key = new byte[]{
+        byte[] key = {
                 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
                 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10
         };
@@ -193,7 +194,7 @@ public class EncryptTest extends Testable {
 
     @Test
     public void test19() {
-        byte[] key = new byte[]{
+        byte[] key = {
                 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
                 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10
         };
@@ -207,10 +208,10 @@ public class EncryptTest extends Testable {
 
     @Test
     public void test20() {
-        byte[] cipher = new byte[] {
+        byte[] cipher = {
                 126, -87, 82, -22, -70, 91, 98, -12, 70, 15, 43, 9, -37, 102, -99, 125
         };
-        byte[] key = new byte[]{
+        byte[] key = {
                 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
                 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10
         };
@@ -236,7 +237,7 @@ public class EncryptTest extends Testable {
 
     @Test
     public void test22() {
-        byte[] cipher = new byte[] {
+        byte[] cipher = {
                 -28, 69, -34, -126, -127, -114, -6, 32, -13, -45, -102, 32, 30, -85, -51
         };
         String key = "123456";
@@ -247,5 +248,27 @@ public class EncryptTest extends Testable {
         byte[] plain = decrypt.decrypt(cipher);
         logger.info(ArrayUtils.output(plain));
         logger.info(new String(plain));
+    }
+
+    @Test
+    public void test23() {
+        byte[] key = {
+                (byte)0xa0, (byte)0x33, (byte)0xd6, (byte)0x78,
+                (byte)0x6b, (byte)0x05, (byte)0x14, (byte)0xac,
+                (byte)0xfc, (byte)0x3d, (byte)0x8e, (byte)0x2d,
+                (byte)0x6a, (byte)0x2c, (byte)0x27, (byte)0x1d
+        };
+        byte[] data = {
+                126, -87, 82, -22, -70, 91, 98, -12, 70, 15, 43, 9, -37, 102, -99, 125
+        };
+
+        Rabbit rabbit = new Rabbit();
+        rabbit.setKey(key);
+
+        byte[] cipher = new byte[16];
+        // -55, 33, 24, 44, 91, 17, -68, -121, -58, -8, 83, -43, -106, 79, 126, 60
+
+        rabbit.cipher(data, cipher, data.length);
+        logger.info(ArrayUtils.output(cipher));
     }
 }
