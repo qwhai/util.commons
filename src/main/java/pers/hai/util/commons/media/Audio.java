be1dc4e7464f860f0f548da2c607c64881b00b4c
Q-WHai <return_zero0@163.com>
2019-05-22 17:39:36 +0800

    feat:添加多媒体模块（逻辑未实现）

---
+++ src/main/java/pers/hai/util/commons/media/Audio.java
@@ -0,0 +1,13 @@
+package pers.hai.util.commons.media;
+
+/**
+ * TODO
+ * <p>
+ * Create Time: 2019-05-22 17:36
+ * Last Modify: 2019-05-22
+ *
+ * @author Q-WHai
+ * @see <a href="https://github.com/qwhai">https://github.com/qwhai</a>
+ */
+public class Audio {
+}
---
+++ src/main/java/pers/hai/util/commons/media/readme.md
@@ -0,0 +1,5 @@
+## pers.hai.util.commons.media
+
+> 处理所有与多媒体相关的操作
+>
+> 未完待续
